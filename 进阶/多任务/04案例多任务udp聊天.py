41108bd273d148b5509b5039c41ba1ee6c4d170f
杨小伟 <2389469307@qq.com>
2020-09-20 00:15:23 +0800

    update:多任务聊天器

---
+++ 进阶/多任务/04案例多任务udp聊天.py
@@ -0,0 +1,35 @@
+import socket
+import threading
+
+
+def recv_msg(udp_so):
+    #     receive
+    while True:
+        rec_data = udp_so.recvfrom(1024)
+        print('recv Msg',rec_data)
+
+
+def send_msg(udp_so, dest_ip, dest_port):
+    ''''send msg'''
+    while True:
+        send_data = input('send msg:')
+        udp_so.sendto(send_data.encode('utf-8'), (dest_ip, dest_port))
+
+
+def main():
+    ''''udp聊天'''
+    udp_so = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #     bind
+    udp_so.bind(('', 7890))
+    #     get he ip
+    dest_ip = input('pleas IP：')
+    dest_port = int(input('please port:'))
+
+    t_recv = threading.Thread(target=recv_msg, args=(udp_so,))
+    t_send = threading.Thread(target=send_msg, args=(udp_so, dest_ip, dest_port))
+
+    t_recv.start()
+    t_send.start()
+
+if __name__ == '__main__':
+    main()
