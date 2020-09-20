9b149805afeb736406b57574515b9b797658b81c
杨小伟 <2389469307@qq.com>
2020-09-20 23:11:36 +0800

    update: copy file add progress

--- 进阶/多任务/进程/03copyDir.py
+++ 进阶/多任务/进程/03copyDir.py
@@ -2,7 +2,7 @@ import os
 import multiprocessing
 
 
-def copy_file(file_name, old_folder_name, new_folder_name):
+def copy_file(q, file_name, old_folder_name, new_folder_name):
     """"完成文件的复制"""
     old_f = open(f'{old_folder_name}/{file_name}', 'rb')
     content = old_f.read()
@@ -11,6 +11,8 @@ def copy_file(file_name, old_folder_name, new_folder_name):
     new_f = open(f'{new_folder_name}/{file_name}', 'wb')
     new_f.write(content)
     new_f.close()
+
+    q.put(file_name)
 
 
 def main():
@@ -27,15 +29,22 @@ def main():
     # 4.create processing pool
     pool = multiprocessing.Pool(5)
 
-    # 5.to pool add copy file plan
+    # 5.create a queue
+    q = multiprocessing.Manager().Queue()
+
+    # 6.to pool add copy file plan
     for file_name in file_names:
-        pool.apply_async(copy_file, args=(file_name, old_folder_name, new_folder_name))
+        pool.apply_async(copy_file, args=(q, file_name, old_folder_name, new_folder_name))
     pool.close()
-    pool.join()
-    # copy old file to new dir
-
-
-    pass
+    # pool.join()
+    all_fil_num = len(file_names)
+    copy_ok_num = 0
+    while True:
+        file_name = q.get()
+        copy_ok_num += 1
+        print(f'\r已完成{(copy_ok_num/all_fil_num) * 100}% ', end='')
+        if copy_ok_num >= all_fil_num:
+            break
 
 
 if __name__ == '__main__':
