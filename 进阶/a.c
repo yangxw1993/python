120005980f8aaac3d951ca4e8f8321874eb5add4
杨小伟 <2389469307@qq.com>
2020-11-08 22:33:53 +0800

    C语言事例

---
+++ 进阶/a.c
@@ -0,0 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2020-11-04 23:19:57
+ * @LastEditTime: 2020-11-04 23:29:04
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /python_base/进阶/多任务/a.c
+  gcc a.c 编译 => a.out => 执行 ./a.out
+ */
+#include <stdio.h>
+int main(){
+  printf("hello world");
+  return 0;
+}
